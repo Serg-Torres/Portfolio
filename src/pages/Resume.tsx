ca3f569625978a3cd3b9d8973d9145ed51cbb2b2
Serg-Torres <torressergio1357@gmail.com>
2024-12-25 00:14:11 -0500

    moved resume style from .tsx to .css'

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Layout, Button } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
-
+import '../styles/Resume.css'
 const { Content } = Layout;
 
 const Resume = () => {
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Content style={{ padding: '20px', display: 'flex', flexDirection: 'column', backgroundColor: 'black', alignItems: 'center', position: 'relative' }}>
-        <div style={{ textAlign: 'center', maxWidth: '600px', marginBottom: '20px', zIndex: 10, position: 'relative' }}>
+    <Layout className = 'layoutStyle'>
+      <Content className = 'contentStyle'>
+        <div className = 'buttonDiv'>
           <Button
             type="primary"
             icon={<DownloadOutlined />}
@@ -19,10 +19,10 @@ const Resume = () => {
             Download Resume
           </Button>
         </div>
-        <div style={{ textAlign: 'center', maxWidth: '1000px', width: '100%', display: 'flex', justifyContent: 'center' }}>
+        <div className = 'resumeDiv'>
           <iframe
             src="https://drive.google.com/file/d/14SQw423qcNcnwCwtQnLeuEv3MzZosRty/preview"
-            style={{ width: '100%', height: '1500px', border: 'none' }}
+            className = 'resumeIFrame'
             title="Resume Preview"
           ></iframe>
         </div>
